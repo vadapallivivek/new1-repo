fc502f19d3bcd9b9ef5c3b35497a4402599df9c0
root <root@ip-172-31-80-195.ec2.internal>
2025-01-25 14:26:05 +0000

    frst-repo

---
+++ ap.java
@@ -0,0 +1,8 @@
+Anyone from an IT or Non-IT background
+can apply
+Students
+
+Experts
+Those wishing to enroll in this course will
+be required to register through our website
+www.frontlinesedutech.
